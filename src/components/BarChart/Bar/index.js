cc9d378c44b73ee4b77cde2a4729327a4613dd27
Vladimir <guiderwg@gmail.com>
2021-04-06 22:05:13 +0400

    added some files in ignore list

--- src/components/BarChart/Bar/index.js
+++ src/components/BarChart/Bar/index.js
@@ -1,11 +1,6 @@
-import React from "react";
-
 const Bar = ({ barHeight }) => {
   const style = {
     height: barHeight,
-    width: "100%",
-    background: "#468EC1",
-    margin: "0 10px",
   };
 
   return <div className="barItem" style={style} />;
